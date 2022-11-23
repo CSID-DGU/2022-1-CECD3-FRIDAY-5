9fdb9dc125537821724b85f539abb9f5bd8be733
ykmoon04 <moonsc014@gmail.com>
2022-11-23 23:08:52 +0900

    Add: 일기 생성

--- Friday Diary/Assets/Scripts/Common/Diary.cs
+++ Friday Diary/Assets/Scripts/Common/Diary.cs
@@ -8,23 +8,24 @@ public class Diary
     public string id;    
     public string text;
     public string targetDate;
+    public double happiness, sadness, disgust, angry, surprise, fear, neutral;
     public DiaryResult diaryResult;
-    public Diary(){
-        id ="user";
-        text = "일기";
-    }
-
-    public Diary(string id, string text)
-    {
-        this.id = id;
-        if(text.Contains("\n")){
-            text = text.Replace("\n","\n\n");
-        }
-        this.text = text;
+    public void InitDiaryResult(){
+        diaryResult = new DiaryResult(this.happiness, this.sadness, this.disgust, this.angry, this.surprise, this.fear, this.neutral);
     }
 }
 
 [System.Serializable]
 public class DiaryResult{
-    public double happiness, sadness, disgust, angry, surprise, neutral;
+    public double happiness, sadness, disgust, angry, surprise, fear, neutral;
+    public DiaryResult(){}
+    public DiaryResult(double happiness, double sadness, double disgust, double angry, double surprise, double fear, double neutral){
+        this.happiness = happiness;
+        this.sadness = sadness;
+        this.disgust = disgust;
+        this.angry = angry;
+        this.surprise = surprise;
+        this.fear = fear;
+        this.neutral = neutral;
+    }
 }
---
+++ Friday Diary/Assets/Scripts/System/Login/LoginSceneScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using System.Text;
+using UnityEngine.SceneManagement;
+
+public class LoginSceneScript : MonoBehaviour
+{
+    public InputField passwordInput, idInput; 
+    public void onSignUpBtnclick()
+    {
+        SceneManager.LoadScene("signin");
+    }
+    
+    public void onLoginBtnclick()
+    {
+        Backend.i?.Login(idInput.text, passwordInput.text, onLoginSuccess); 
+    }
+
+    public void onLoginSuccess(User user){
+        GameManager.i.SetUser(user);
+
+        SceneManager.LoadScene("Main");
+    }
+}
